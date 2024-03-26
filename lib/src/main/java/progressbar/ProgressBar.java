2aed1bd987a08b86393238be835aea52f1e22961
Thorben Linker <thorbenlinker@gmail.com>
2024-03-26 23:44:53 +0100

    feat: implement interface; implement simple progress bar

---
+++ lib/src/main/java/progressbar/ProgressBar.java
@@ -0,0 +1,14 @@
+package progressbar;
+
+public interface ProgressBar {
+
+    //configurationMethods
+
+
+    //interface methods
+    void step();
+
+    void step(int steps);
+
+    void print();
+}
---
+++ lib/src/main/java/progressbar/SimpleProgressBar.java
@@ -0,0 +1,60 @@
+package progressbar;
+
+public class SimpleProgressBar implements ProgressBar{
+
+    private final int target;
+    private final int percentagePerStep;
+    private int progress;
+    private double percentage;
+    private String state;
+    protected SimpleProgressBar(final int target, final int percentagePerStep){
+        this.target = target;
+        this.percentagePerStep = percentagePerStep;
+        progress = 0;
+        percentage = 0.0;
+    }
+    @Override
+    public void step() {
+        step(1);
+    }
+
+    @Override
+    public void step(int steps) {
+        progress += steps;
+        calculatePercentage();
+        createState();
+    }
+
+    @Override
+    public void print() {
+        System.out.print(state);
+    }
+
+    //utility methods
+    private void calculatePercentage(){
+        percentage = ((double) target / progress) * 100;
+    }
+
+    private void createState(){
+        StringBuilder builder = new StringBuilder("[");
+        double remainder = percentage;
+        int countOfSteps = 0;
+        //set already completed progress visually
+        while (remainder >= percentagePerStep) {
+            builder.append("#");
+            remainder -= percentagePerStep;
+            countOfSteps++;
+        }
+
+        //set remaining progress visually
+        remainder = 100 - (countOfSteps * percentagePerStep);
+        countOfSteps = Math.ceilDiv((int)remainder, percentagePerStep);
+
+        builder.append("..".repeat(Math.max(0, countOfSteps)))
+                .append("] ")
+                .append(percentage)
+                .append("%");
+
+        state = builder.toString();
+    }
+}
